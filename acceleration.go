743a9dfe2bb7ab561eb21b93ecb55947c01ae697
Tomas Ohlson <ohlsont@users.noreply.github.com>
2019-06-05 12:29:01 +0200

    Add helperfunction to acceleration
    To convert accelretation for use by other libraries a helper
    function to get out a pure float value is needed.

--- acceleration.go
+++ acceleration.go
@@ -5,3 +5,7 @@ type Acceleration float64
 const (
 	MetersPerSecondSquared Acceleration = 1
 )
+
+func (acceleration Acceleration) MetersPerSecondSquared() float64 {
+	return float64(acceleration)
+}

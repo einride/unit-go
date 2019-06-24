d12685aab607f577db785fe3df8e8311257c75a6
Oscar Söderlund <odsod@users.noreply.github.com>
2019-06-24 06:03:28 +0200

    Add prefixes
    As inspired by gonum.org/unit.

--- distance_test.go
+++ distance_test.go
@@ -8,14 +8,10 @@ import (
 
 func TestDistance(t *testing.T) {
 	require.InDelta(t, 1000/milesConstant, (1 * Mile).Metres(), 0.0000000001)
-	require.Equal(t, 1000., 1*KiloMetre.Metres())
-	require.Equal(t, 1., 1*Metre.Metres())
+	require.Equal(t, 1000., (Kilo * Metre).Metres())
+	require.Equal(t, 1., Metre.Metres())
 }
 
-func TestSpeed_KiloMetre(t *testing.T) {
-	require.Equal(t, 1., (1 * KiloMetre).KiloMetres())
-}
-
-func TestSpeed_Mile(t *testing.T) {
+func TestDistance_Mile(t *testing.T) {
 	require.InDelta(t, 1., (1 * Mile).Miles(), 0.0000000000001)
 }
